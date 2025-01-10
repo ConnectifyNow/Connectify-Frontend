d25f114b897c8de4cbbeb2da50b90d5387c33049
Hila <hila.ohana1910@gmail.com>
2025-01-10 13:36:06 +0200

    get user from server

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,14 @@
 import { AxiosResponse } from "axios";
-import { User } from "../types/User";
+import { User } from "../types/user";
 import { headers } from "./authService";
 import apiClient from "./httpCommon";
 
 export const getUsers = async (): Promise<AxiosResponse<User[]>> => {
-    return await apiClient.get(`/users`, { headers: headers() });
+  return await apiClient.get(`/users`, { headers: headers() });
 };
 
-export const getUserById = async (userId: User['_id']): Promise<AxiosResponse<User>> => {
-    return await apiClient.get(`/users/${userId}`, { headers: headers() });
+export const getUserById = async (
+  userId: User["_id"]
+): Promise<AxiosResponse<User>> => {
+  return await apiClient.get(`/users/${userId}`, { headers: headers() });
 };
