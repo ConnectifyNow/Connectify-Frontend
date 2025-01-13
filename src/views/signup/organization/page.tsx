52427bcfd23a6860a3ef067850eef91e36f7f728
Hila <hila.ohana1910@gmail.com>
2025-01-13 23:10:56 +0200

    fix/change-url-to-upload-img

--- src/views/signup/organization/page.tsx
+++ src/views/signup/organization/page.tsx
@@ -12,6 +12,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { ImageIcon } from "lucide-react";
 import useUserStore from "@/stores/setUserStore";
 import CustomSelect from "../../../components/shared/customSelect";
 import { getAiDescription } from "@/services/aiService";
@@ -190,17 +191,20 @@ export default function OrganizationSignUpPage() {
                 required
               />
             </div>
-            <div className="space-y-2">
-              <Label htmlFor="websiteLink">Organization URL</Label>
-              <Input
-                id="websiteLink"
-                name="websiteLink"
-                type="url"
-                value={formData.websiteLink}
-                onChange={handleChange}
-                placeholder="https://www.yourorganization.com"
-                required
-              />
+            <div>
+              <Label htmlFor="websiteLink">Organization image</Label>
+              <label
+                htmlFor="image"
+                className="flex flex-col items-center justify-center w-full h-32 border-2 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                style={{ marginTop: "5px" }}
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <ImageIcon className="w-8 h-8 mb-4 text-gray-500" />
+                  <p className="mb-2 text-sm text-gray-500">
+                    <span className="font-semibold">Click to upload</span>
+                  </p>
+                </div>
+              </label>
             </div>
             <div className="space-y-2">
               <Label htmlFor="city">City</Label>
@@ -245,16 +249,22 @@ export default function OrganizationSignUpPage() {
                 ? `Wait ${coolDownTime}s`
                 : "Generate Description using AI"}
             </Button>
-            <div className="space-y-2">
-              <Label htmlFor="imageUrl">Organization Logo URL</Label>
-              <Input
-                id="imageUrl"
-                name="imageUrl"
-                type="url"
-                value={formData.imageUrl}
-                onChange={handleChange}
-              />
+            <div>
+              <Label htmlFor="websiteLink">Organization Logo</Label>
+              <label
+                htmlFor="image"
+                className="flex flex-col items-center justify-center w-full h-32 border-2 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                style={{ marginTop: "5px" }}
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <ImageIcon className="w-8 h-8 mb-4 text-gray-500" />
+                  <p className="mb-2 text-sm text-gray-500">
+                    <span className="font-semibold">Click to upload</span>
+                  </p>
+                </div>
+              </label>
             </div>
+
             <Button type="submit" className="w-full" disabled={isLoading}>
               {isLoading ? "Completing Sign Up..." : "Complete Sign Up"}
             </Button>
